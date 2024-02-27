6c66e648629411a5c8b55c1f2484c1e536d91aa1
bap2pecs <bap2pecs@proton.me>
2024-02-27 08:48:25 -0700

    improvements

--- deployment/helpers/approve.js
+++ deployment/helpers/approve.js
@@ -1,36 +1,45 @@
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-console */
 /*
-Usage: npm run deploy:approve:matic <network>
-*/
+ *Usage: npm run deploy:approve:matic <network>
+ */
 
-
-const { ethers } = require("hardhat");
+const { ethers } = require('hardhat');
 require('dotenv').config();
 
+const deployOutput = require('../../deploymentOutput/deploy_output.json');
+
 async function main() {
-  // Load provider
-  let provider = ethers.provider;
+    // Load provider
+    const { provider } = ethers;
 
-  // Load signer
-  let signer = (await ethers.getSigners())[0];
+    // Load signer
+    const signer = (await ethers.getSigners())[0];
 
-  const maticTokenFactory = await ethers.getContractFactory(
-    "ERC20PermitMock",
-    provider
-  );
-  maticTokenContract = maticTokenFactory.attach(
-    // From deployments/.../deploy_output.json maticTokenAddress
-    process.env.MATIC_TOKEN_ADDRESS
-  );
-  maticTokenContractWallet = maticTokenContract.connect(signer);
-  const res = await maticTokenContractWallet.approve(
-  // From deployments/.../deploy_output.json cdkValidiumAddress
-    process.env.CDK_VALIDIUM_ADDRESS,
-    ethers.utils.parseEther("100.0")
-  );
-  console.log(res);
+    const maticTokenFactory = await ethers.getContractFactory(
+        'ERC20PermitMock',
+        provider,
+    );
+    const maticTokenContract = maticTokenFactory.attach(
+        deployOutput.maticTokenAddress,
+    );
+    const maticTokenContractWallet = maticTokenContract.connect(signer);
+    const tx = await maticTokenContractWallet.approve(
+        deployOutput.cdkValidiumAddress,
+        ethers.utils.parseEther('100.0'),
+    );
+    console.log('Transaction hash:', tx.hash);
+    // Wait for receipt
+    const receipt = await tx.wait();
+    console.log('Transaction confirmed in block:', receipt.blockNumber);
 }
 
 // to prevent execution by accident on import
 if (require.main === module) {
-    main();
+    main()
+        .then(() => process.exit(0))
+        .catch((error) => {
+            console.error(error);
+            process.exit(1);
+        });
 }
