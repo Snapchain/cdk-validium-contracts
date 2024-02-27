b12d154097ffe6670b6f810cf0a46c6a0add93fe
bap2pecs <bap2pecs@proton.me>
2024-02-26 20:05:12 -0700

    improve wallets.js
    generate accounts with the same mnemonic

--- deployment/helpers/wallets.js
+++ deployment/helpers/wallets.js
@@ -1,39 +1,44 @@
-const ethers = require("ethers");
+const ethers = require('ethers');
 const fs = require('fs');
 
 async function main() {
-  const password = process.argv[2];
-  
-  if (!password) {
-    console.error('Please provide a password as an argument.');
-    process.exit(1);
-  }
+    const password = process.argv[2];
 
-  const arrayNames = [
-    "## Deployment Address",
-    "\\n\\n## Trusted sequencer",
-    "\\n\\n## Trusted aggregator",
-    "\\n\\n## DAC member",
-    "\\n\\n## Claim tx manager",
-  ];
+    if (!password) {
+        console.error('Please provide a password as an argument.');
+        process.exit(1);
+    }
 
-  let output = '';
-  for (let i = 0; i < arrayNames.length; i++) {
-    const wallet = ethers.Wallet.createRandom();
-    output += `${arrayNames[i]}\n`;
-    output += `Address: ${wallet.address}\n`;
-    output += `PrvKey: ${wallet.privateKey}\n`;
-    output += `mnemonic: "${wallet.mnemonic.phrase}"\n`;
+    const arrayNames = [
+        '## Deployment Address',
+        '\\n\\n## Trusted sequencer',
+        '\\n\\n## Trusted aggregator',
+        '\\n\\n## DAC member',
+        '\\n\\n## Claim tx manager',
+    ];
 
-    const keystoreJson = await wallet.encrypt(password);
-    output += `keystore: ${keystoreJson}\n\n`;
-  }
+    // Generate a single mnemonic for all wallets
+    const mnemonic = ethers.Wallet.createRandom().mnemonic.phrase;
 
-  fs.writeFileSync('wallets.txt', output);
-  console.log('Output written to wallets.txt');
+    let output = `Mnemonic: ${mnemonic}\n\n`;
+    for (let i = 0; i < arrayNames.length; i++) {
+    // Use the same mnemonic for each wallet but derive a new path
+        const path = `m/44'/60'/0'/0/${i}`;
+        const wallet = ethers.Wallet.fromMnemonic(mnemonic, path);
+
+        output += `${arrayNames[i]}\n`;
+        output += `Address: ${wallet.address}\n`;
+        output += `PrvKey: ${wallet.privateKey}\n`;
+
+        const keystoreJson = await wallet.encrypt(password);
+        output += `keystore: ${keystoreJson}\n\n`;
+    }
+
+    fs.writeFileSync('wallets.txt', output);
+    console.log('Output written to wallets.txt');
 }
 
 main().catch((e) => {
-  console.error(e);
-  process.exit(1);
+    console.error(e);
+    process.exit(1);
 });
